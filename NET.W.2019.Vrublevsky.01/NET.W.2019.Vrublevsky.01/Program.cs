39ec4a0dfa6a974d9a34a2ca87f76d630f0b244e
Izigraim <z7635332@gmail.com>
2019-11-13 21:03:27 +0300

    final version

---
+++ NET.W.2019.Vrublevsky.01/NET.W.2019.Vrublevsky.01/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.W._2019.Vrublevsky._01
+{
+    class Program
+    {
+        static void QuickSort(int[] array, int first, int last)
+        {
+            if(first < last)
+            {
+                int left = first, right = last, middle = array[(left + right) / 2];
+                do
+                {
+                    while (array[left] < middle) left++;
+                    while (array[right] > middle) right--;
+                    if (left <= right)
+                    {
+                        int temp = array[left];
+                        array[left] = array[right];
+                        array[right] = temp;
+                        left++;
+                        right--;
+                    }
+                } while (left <= right);
+                if (first < right) QuickSort(array, first, right);
+                if (left < last) QuickSort(array, left, last);
+            }
+        }
+
+        static void MergeSort(int[] array, int left, int right)
+        {
+            int mid;
+
+            if(right > left)
+            {
+                mid = (right + left) / 2;
+                MergeSort(array, left, mid);
+                MergeSort(array, (mid + 1), right);
+                Merge(array, left, (mid + 1), right);
+            }
+        }
+
+        static void Merge(int[] array, int left, int mid, int right)
+        {
+            int[] temp = new int[array.Length];
+            int leftEnd = mid - 1, length = right - left + 1, pos = left;
+            while((left <= leftEnd) && (mid <= right))
+            {
+                if(array[left] <= array[mid])
+                {
+                    temp[pos++] = array[left++];
+                }
+                else
+                {
+                    temp[pos++] = array[mid++];
+                }
+            }
+
+            while(left <= leftEnd)
+            {
+                temp[pos++] = array[left++];
+            }
+            while(mid <= right)
+            {
+                temp[pos++] = array[mid++];
+            }
+            for(int i = 0; i <length; i++)
+            {
+                array[right] = temp[right];
+                right--;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int[] arrayQS = new int[rand.Next(0, 30)];
+            int[] arrayMerge = new int[arrayQS.Length];
+
+            for (int i = 0; i < arrayQS.Length; i++)
+                arrayQS[i] = rand.Next(0, 100);
+
+            Array.Copy(arrayQS, arrayMerge, arrayQS.Length);
+
+            QuickSort(arrayQS, 0, arrayQS.Length - 1);
+
+            Console.WriteLine("QuickSort: ");
+            for (int i = 0; i < arrayQS.Length; i++)
+                Console.Write("{0} ", arrayQS[i]);
+            MergeSort(arrayMerge, 0, arrayMerge.Length - 1);
+            Console.WriteLine("\nMergeSort: ");
+            for (int i = 0; i < arrayQS.Length; i++)
+                Console.Write("{0} ", arrayMerge[i]);
+        }
+    }
+}
